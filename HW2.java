2552a9621b215c0b4a51a2049d47a5a656eb7f7a
hsu1997118 <32082108+hsu1997118@users.noreply.github.com>
2017-10-27 11:55:36 +0800

    B0544113

--- HW2.java
+++ HW2.java
@@ -3,7 +3,12 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * @author TODO: please add student ID and name here
+ * @author TODO: please add student ID and name here 
+ * B0544113 許芮萍
+ * 步驟一 由Card開始寫 suit=花色 rank=數字 
+ * 步驟二 再寫Deck 將nDeck牌放入cards中
+ * 因為不知道會有多少牌 所以要用for(Card card:cards))不要用for(int i...;i... ;i...)
+ * 最後將牌依序印出來
  * Try to write some comments for your codes (methods, 15 points)
  */
 public class HW2 {
@@ -65,6 +70,8 @@ public class HW2 {
 }
 /**
  * Description: TODO: please add description here
+ * nDeck為使用者輸入，知道nDeck為多少後，將它放入三層for迴圈中的第一層，表示種共有幾副牌
+ * 三層for迴圈是要計算總共有多少牌
  */
 class Deck{
 	private ArrayList<Card> cards;
@@ -77,13 +84,24 @@ class Deck{
 		//Card card=new Card(1,1); ->means new card as clubs ace
 		//cards.add(card);
 		//Sample code end
+		for(int k=0 ; k<nDeck ; k++){
+			for(int i=1; i<=4; i++){
+				for(int j=1; j<=13; j++){
+					Card card=new Card(i,j);
+					cards.add(card);
+				}
+			}
+		}
 
 	}	
 	//TODO: Please implement the method to print all cards on screen (10 points)
 	public void printDeck(){
 		//Hint: print all items in ArrayList<Card> cards, 
 		//TODO: please implement and reuse printCard method in Card class (5 points)
-
+		for(Card card:cards)
+		{
+			card.printCard();
+		}
 	}
 	public ArrayList<Card> getAllCards(){
 		return cards;
@@ -91,6 +109,7 @@ class Deck{
 }
 /**
  * Description: TODO: please add description here
+ *在printCard中用if和else if寫入如何判斷suit和rank
  */
 class Card{
 	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
@@ -106,7 +125,33 @@ class Card{
 	//TODO: 1. Please implement the printCard method (20 points, 10 for suit, 10 for rank)
 	public void printCard(){
 		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
-
+		if(suit==1) {
+			System.out.print("Clubs");
+		}
+		else if(suit==2) {
+			System.out.print("Diamonds");
+		}
+		else if(suit==3) {
+			System.out.print("Hearts");
+		}
+		else if(suit==4) {
+			System.out.print("Spades");
+		}
+		if(rank==1) {
+			System.out.println(" Ace");
+		}
+		else if(rank==11) {
+			System.out.println(" Jack");
+		}
+		else if(rank==12) {
+			System.out.println(" Queen");
+		}
+		else if(rank==13) {
+			System.out.println(" King");
+		}
+		else
+			System.out.println(" "+rank);
+		
 	}
 	public int getSuit(){
 		return suit;
